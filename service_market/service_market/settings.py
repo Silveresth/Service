8969c1437ab8b15e807d043119348a1aaae46c02
Silvere <sisisoumdina@gmail.com>
2026-05-14 12:34:41 +0000

    new settings

--- service_market/service_market/settings.py
+++ service_market/service_market/settings.py
@@ -146,16 +146,6 @@ DATABASES = {
     )
 }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME', default='sm_db'),
-        'USER': config('DB_USER', default='postgres'),
-        'PASSWORD': config('DB_PASSWORD', default=''),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432'),
-    }
-}
 
 # Internationalization
 LANGUAGE_CODE = 'fr-fr'
