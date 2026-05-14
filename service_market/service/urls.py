c043b895756777786cbd8e05a2e6025f77b4ff9c
Silvere <sisisoumdina@gmail.com>
2026-05-14 11:00:16 +0000

    Railway fix

--- service_market/service/urls.py
+++ service_market/service/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
 from .views import (
     AuthViewSet, CompteViewSet, ServiceViewSet, ReservationViewSet,
@@ -27,4 +28,5 @@ urlpatterns = router.urls + [
     path('fidelite/mes-points/',    mes_points_fidelite,  name='fidelite-points'),
     path('fidelite/ajouter-points/', ajouter_points,      name='fidelite-ajouter-points'),
     path('admin/rapport-pdf/',      rapport_pdf_admin,    name='admin-rapport-pdf'),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
--- service_market/service_market/settings.py
+++ service_market/service_market/settings.py
@@ -137,6 +137,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'frontend' / 'build'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -170,6 +171,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'frontend' / 'build' / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
