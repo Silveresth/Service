250b80d4dd7611f2669dd4f871e64a67af5b1bfa
Silvere <sisisoumdina@gmail.com>
2026-06-16 00:37:08 +0000

    Final cleanup: Fix ASGI initialization and ensure secure config

--- service_market/service_market/asgi.py
+++ service_market/service_market/asgi.py
@@ -2,18 +2,18 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'service_market.settings')
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import service.routing  # WebSocket routing
 
+# Initialize Django ASGI app first - this also calls django.setup()
 django_asgi_app = get_asgi_application()
 
+# Import routing AFTER django is fully initialized
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from service.routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter({
-  "http": django_asgi_app,
-  "websocket": AuthMiddlewareStack(
-        URLRouter(
-            service.routing.websocket_urlpatterns
-        )
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
     ),
 })
-
